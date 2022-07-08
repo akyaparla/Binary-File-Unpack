e987a9c978e18ea50eb29fb71cb47a0eccd56914
akyaparla <52508622+akyaparla@users.noreply.github.com>
2022-07-08 14:01:39 -0400

    Delete loaddata.m

--- loaddata.m
+++
@@ -1,9 +0,0 @@
-function [data, t] = loaddata(fileName, numChan, fs)
-    fd = fopen(fileName, "rb");
-    data = fread(fd, [numChan, inf], "float32");
-    fclose(fd);
-    dt = 1/fs;
-    t = 0:dt:(length(data)-1)*dt;
-end
-
-    
