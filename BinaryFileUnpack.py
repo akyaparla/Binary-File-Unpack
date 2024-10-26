0108328fb145cb72c85f11f7974550dd71da537b
Akshat Yaparla <52508622+akyaparla@users.noreply.github.com>
2024-10-26 18:14:19 -0400

    Add files via upload
    Reflects important updates to BinaryFileUnpack.py over the past year.

--- BinaryFileUnpack.py
+++ BinaryFileUnpack.py
@@ -1,14 +1,16 @@
 import numpy as np
 import csv
+import warnings
 class BinaryFileUnpack:
     '''
     Class to neatly unpack binary files. Provides the header information as well as the data for the sensors.
 
     Methods:
         __init__: Method to initialize a BinaryFileUnpack object.
-        spectra (np.ndarray): Returns the frequencies (in Hertz) and the power (in deciBels) present in the spectrum of the data. Utilizes the scipy module to return the spectrum.
-        plot_static: Outputs a static plot of the sensor data against a time series. Utilizes the matplotlib module.
-        plot_interactive: Outputs a plot of the sensor data against a time series. Implements a HoverTool and a CrossHairTool for the user to analyze the data.
+        spectra (np.ndarray): Returns a periodogram of the data.
+        spectrogram (np.ndarray): Returns the frequencies (in Hertz) and the power (in deciBels) as a function of a time series.
+        plot_static: Outputs a static plot of the sensor data against a time series. Utilizes the Matplotlib module.
+        plot_interactive (depreciated): Outputs a plot of the sensor data against a time series. Implements a HoverTool and a CrossHairTool for the user to analyze the data. Utilizes the Bokeh module.
         plot_eruption_PT: Outputs a Pressure-Temperature plot of an eruption. Utilizes the matplotlib module.
         getTimeRange: Returns a time series of the data. Intended for use as a time series input for other relavent methods.
         getDtype (np.ndarray or Object): Method to read the bytes of a file as a given data type. Main purpose is to be used in __init__ method.
@@ -203,9 +205,8 @@ class BinaryFileUnpack:
                             for j in range(devCount):
                                 sens_used[newOrder[j]] = int(line[j+1])
                 
-            except:
+            except FileNotFoundError:
                 # No header file, use default sens and send warning
-                import warnings
                 msg = "\n"
                 for j in range(self.num_sens):
                     msg += f"\tPosition {j+1}: SN {sens_used[j]}\n"
@@ -272,6 +273,7 @@ class BinaryFileUnpack:
 
     def spectra(self, data_spec: np.ndarray) -> np.ndarray:
         '''
+        WARNING: Depreciated
         Returns the frequencies (in Hertz) and the power (in deciBels) present in the spectrum of the data. Utilizes the scipy module to return the spectrum.
 
         Parameters:
@@ -279,10 +281,16 @@ class BinaryFileUnpack:
 
         Returns:
             Pxx (ndarray): The resulting 3-dimensional array storing frequency and power data of the complete spectrum.
+
                 First axis: Index 0 is frequency data, index 1 is power data.
+
                 Second axis: Contains the sensor indices.
+
                 Third axis: The data of either frequency or power for the respective sensor.
+        Raises:
+            FutureWarning: Depreciated
         '''
+        warnings.warn("Soon to be depreciated", FutureWarning)
         # Getting Frequencies for Spectral Analysis
         import scipy.signal as signal
         # Length of first axis indicates number of sensors
@@ -303,6 +311,59 @@ class BinaryFileUnpack:
             Pxx[:, i, :] = np.stack([f, power])
 
         return Pxx
+    
+    def spectrogram(self, data:np.ndarray, window:int=20, times:np.ndarray=None) -> np.ndarray:
+        '''
+        Returns spectogram of the data with indicated window length. Taken at every second of the data.
+
+        Parameters:
+            data (ndarray): 1-D array of data to analyze.
+            window (int): Time interval to use for FFT at each second.
+            times (ndarray): Time series used for the data. Default is entire time series.
+
+        Returns:
+            spec (ndarray): The resulting 3-dimensional array storing frequency and power data of the complete spectrum.
+
+                First axis: Time series of data, at every second.
+
+                Second axis: Frequency or Power data, obtained by scipy.fft.rfftfreq or scipy.fft.rfft, respectively.
+
+                Third axis: 0 or 1, if 0, second axis is frequency data, if 1, second axis power data.
+        Raises:
+            ValueError: Window out of length or data and time series length do not match
+        '''
+        import scipy.fft as fft
+        # FFT on Data
+        if times is None:
+            times = self.time
+        if len(data) != len(times):
+            raise ValueError("Data array and time series lengths do not match", data.shape, times.shape)
+        
+        num_samples = int(window*self.fs/2) + 1
+        duration = int(times[-1] - times[0])
+        if window > duration:
+            raise ValueError("Window length is longer than time series duration", window, duration)
+        
+        spec = np.zeros((duration, num_samples, 2))
+        spec[:, :, 0] = fft.rfftfreq(int(window*self.fs), d=1/self.fs)
+        for t in range(1, duration+1):
+            if t < window:
+                data_to_use = data[: int(t * self.fs)]
+            else:
+                data_to_use = data[int((t - window) * self.fs) : int(t * self.fs)]
+            new_spec = fft.rfft(data_to_use)
+            db_spec = 10*np.log10(new_spec)
+            if t < window:
+                freq = fft.rfftfreq(data_to_use.shape[0], d=1/self.fs)
+                j = 0
+                for i in range(spec.shape[1]):
+                    if spec[t-1][i][0] > freq[j]:
+                        j += 1
+                    spec[t-1][i][1] = db_spec[j]
+
+            else:
+                spec[t-1, :, 1] = db_spec
+        return spec
 
     def plot_static(self, x:np.ndarray, y:np.ndarray, x_label:str, y_label:str, plots_shape:tuple, color:str='blue', 
                     y2:np.ndarray=None, y2_label:str=None, color2:str='red', x_axis_type:str='linear', ordering:np.ndarray=None,
@@ -406,156 +467,6 @@ class BinaryFileUnpack:
 
         if figfilename is not None:
             fig.savefig(figfilename,transparent=True)
-
-    def plot_interactive(self, x:np.ndarray, y:np.ndarray, x_label:str, y_label:str, 
-                         plots_shape:tuple, color=None, x_axis_type:str='linear', output_format:str='file'):
-        '''
-        WARNING: Incredibly depreciated!
-        Outputs a plot of the sensor data against a time series. Implements a HoverTool and a CrossHairTool for the user to analyze the data.
-        Parameters:
-            x (ndarray): A (1-D) array of the time series or (2-D) array of the frequency data. If 2-D, axis convention follows that of the param y.
-            y (ndarray): A 2-dimensional array with the sensor data or power spectrum. 
-                First axis: The sensor index.
-                Second axis: Contains the data for that sensor.
-            x_label (str): The name of the data that defines the x-axis.
-            y_label (str): The type of sensor data along with its units.
-            plots_shape (tuple): The shape of the plots in the output in terms of number of rows and columns. Input should be (rows, cols).
-            color (Any): Identifies the line_color feature of each line glyph for the plots. Allowed inputs are those allowed by line_color (default is element in bokeh.palettes.Turbo6).
-            x_axis_type ('linear', 'log'): The scale of the x-axis, either can be a linear axis (='linear;) or logarithmic axis (='log'). Default is 'linear'. 
-            output_format ('file', 'notebook'): Outputting a file in either a .html file (='file') or within the notebook (='notebook'). Default is 'file'.
-        
-        Raises:
-            ValueError: 
-                output_format is not either 'file' or 'notebook'.
-                The plot dimension does not match the number of plots (same as the number of sensors).
-        '''
-        from bokeh.plotting import figure, output_file, output_notebook, reset_output, show, ColumnDataSource
-        from bokeh.layouts import gridplot, column
-        from bokeh.models import CustomJS
-        from bokeh.models.tools import HoverTool, CrosshairTool, BoxZoomTool, WheelZoomTool, SaveTool, ResetTool
-        from bokeh.models.widgets import RangeSlider
-        
-        import warnings
-        warnings.warn("Very Depreciated, do not use")
-
-        if plots_shape[0]*plots_shape[1] != y.shape[0]:
-            raise ValueError(f"Plot dimension does not match number of plots. Plot Dimension: {plots_shape}, Number of plots: {self.num_sens}")
-
-        # Initialize the plots into a numpy array
-        plots = np.empty(self.num_sens, dtype='object')
-        for i in range(self.num_sens):
-            # Determine the x_axis range
-            x_to_use = x if len(x.shape)==1 else x[i]
-            x_range = (x_to_use[0], x_to_use[-1])
-            
-            # Logarithm Case - make sure there are no zeros involved
-            if x_axis_type == 'log':
-                if x_range[0] <= 0:
-                    x_range = (x_to_use[1], x_range[1])
-                
-                x_range = (10**np.floor(np.log10(x_range[0])), 10**np.ceil(np.log10(x_range[1])))
-
-            plots[i] = figure (
-                title = f"Sensor {i+1}",
-                sizing_mode = "stretch_width",
-                plot_height = 675//plots_shape[0],
-                x_range=x_range,
-                x_axis_type=x_axis_type,
-            )
-
-        # Create ColumnDataSources with x and y data for each sensor
-        sources = np.empty(self.num_sens, dtype=ColumnDataSource)
-        for i in range(self.num_sens):
-            sources[i] = ColumnDataSource({'x': x[i] if x.shape[0] == y.shape[0] else x , f'Sensor {i+1}':y[i]})
-
-        # Double-ended range slider so user can select data in a given time frame
-        x_to_use = x if len(x.shape)==1 else x[0]
-        time_range = RangeSlider(start=x_to_use[1], end=x_to_use[-1], value=(x_to_use[1], x_to_use[-1]), step=0.01, sizing_mode="stretch_width")
-        for plot in plots:
-            time_range.js_link('value', plot.x_range, 'start', attr_selector=0)
-            time_range.js_link('value', plot.x_range, 'end', attr_selector=1)
-
-        # Adjust the y-axis for each plot on change of time_range
-        time_range.js_on_change('value', CustomJS(
-            args=dict(plots=plots, sources=sources), 
-            code="""
-            // Getting x-data (shared between all sources)
-            var x_arr = sources[0].data['x'];
-            var x_range = cb_obj.value;
-            // Getting indices of range slider
-            var start_ind = x_arr.indexOf(x_range[0]);
-            var end_ind = x_arr.indexOf(x_range[1]);
-            if (start_ind == -1) {
-                start_ind = 0;
-            }
-            if (end_ind == -1) {
-                end_ind = x_arr.length - 1;
-            }
-            // Iterate through all the sensors
-            for (let i = 0; i < plots.length; i++) {
-                // Sensor number
-                var sensNum = 'Sensor '+(i+1);
-                
-                // Getting y-data
-                var y_arr = sources[i].data[sensNum];
-                var y_range_arr = y_arr.slice(start_ind, end_ind);
-                
-                // Getting max, min, and range of data
-                var max = Math.max(...y_range_arr.filter((el) => !isNaN(el)));
-                var min = Math.min(...y_range_arr.filter((el) => !isNaN(el)));
-                var range = max - min;
-                
-                // Dynamically changing the axis range
-                plots[i].y_range.start = min - range/25;
-                plots[i].y_range.end = max + range/25;
-                console.log(min)
-            }
-            """
-            ))
-
-        if color is None:
-            from bokeh.palettes import Turbo6
-            import random 
-            color = random.choice(Turbo6)
-
-        hover_tools = np.empty(self.num_sens, dtype=object) 
-        for i in range(self.num_sens):
-            hover_tools[i] = HoverTool(
-                tooltips = [
-                    (x_label, f"@x"),
-                    (y_label, f"@{{Sensor {i+1}}}")
-                ],
-                renderers = [plots[i].line(x='x', y=f"Sensor {i+1}", line_color = color, source=sources[i])]
-            )
-            plots[i].tools = [
-                hover_tools[i], 
-                CrosshairTool(dimensions="height", line_alpha=0.5, line_color="purple"), 
-                BoxZoomTool(), WheelZoomTool(), SaveTool(), ResetTool()
-            ]
-
-        # Adding axis labels
-        grid_layout = np.reshape(plots, plots_shape)
-        # x-axis
-        for i in range(grid_layout.shape[1]):
-            grid_layout[-1][i].xaxis.axis_label = x_label
-        # y-axis
-        for i in range(grid_layout.shape[0]):
-            grid_layout[i][0].yaxis.axis_label = y_label
-
-        # Formatting the Graphs
-        grid_plots = gridplot(list(grid_layout), sizing_mode = "stretch_width")
-        grid = column(grid_plots, time_range, sizing_mode="stretch_width")
-
-        reset_output()
-        if output_format == 'file':
-            y_strip = y_label.strip(' \\><:\"|?*$')
-            x_strip = x_label.strip(' \\><:\"|?*$')
-            output_file(f"{self.fileName.split('.')[0]}-{y_strip}-{x_strip}.html")
-        elif output_format == 'notebook':
-            output_notebook()
-        else:
-            raise ValueError(f"{output_format} is not a valid option for output_format. Allowed options are 'file' and 'notebook'")
-        show(grid)
 
     def plot_eruption_PT(self, sharex:bool=True, sharey:bool=True, xlim:tuple=(98, 104), ylim:tuple=(0.95, 1.225), times:np.ndarray=None, title:str=None, 
                          show_phase_boundaries:bool=True, ordering:np.ndarray=np.arange(0, 6, dtype=int), cmap:str='summer', savefig:bool=False):
